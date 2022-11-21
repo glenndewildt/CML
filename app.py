ef537178d903bb8e33d2cfe1c376abe3bb76c210
Glenn de Wildt <glenn@vergelijkgroep.nl>
2022-11-21 13:01:37 +0100

    whaddup

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ def motorstep(x):
         result +=30
     else:
         result +=70
-    return tm
+
+    return result
 
 
 
@@ -59,56 +60,56 @@ def example1():
     return t
 
 
-print(str(example1())+ "ms")
-
 # Q 1D
-def example2():
-    temp = "extreme"
-    if(temp == "extreme"):
-        t = start()
-        t += perceptualstep("fast")
-        t += cognitivestep("fast")
-        t += motorstep("fast")
-        print(t)
+def example2(x):
+    print("hello")
+    if(x == "extreme"):
+        t1 = start()
+        t1 += perceptualstep("fast")
+        t1 += cognitivestep("fast") 
+        t1 += motorstep("fast")
+        print(t1)
 
-        t = start()
-        t += perceptualstep("middle")
-        t += cognitivestep("middle")
-        t += motorstep("middle")
-        print(t)
 
-        t = start()
-        t += perceptualstep("slow")
-        t += cognitivestep("slow")
-        t += motorstep("slow")
-        print(t)
-    elif(temp == "all"):
+        t2 = start()
+        t2 += perceptualstep("middle")
+        t2 += cognitivestep("middle")
+        t2 += motorstep("middle")
+        print(t2)
+
+        t3 = start()
+        t3 += perceptualstep("slow")
+        t3 += cognitivestep("slow")
+        t3 += motorstep("slow")
+        print(t3)
+        return [t1,t2,t3]
+    elif(x == "all"):
         a = start()
-        test1 = []
-        test1.append(perceptualstep("fast"))
-        test1.append(cognitivestep("fast")) 
-        test1.append(motorstep("fast")) 
-        print(t)
+        fast_array = []
+        fast_array.append(perceptualstep("fast"))
+        fast_array.append(perceptualstep("middle")) 
+        fast_array.append(perceptualstep("slow")) 
 
-        t += perceptualstep("middle")
-        t += cognitivestep("middle")
-        t += motorstep("middle")
-        print(t)
+        middle_array = []
+        middle_array.append(cognitivestep("fast"))
+        middle_array.append(cognitivestep("middle")) 
+        middle_array.append(cognitivestep("slow")) 
 
-        t += perceptualstep("slow")
-        t += cognitivestep("slow")
-        t += motorstep("slow")
-        print(t)
+        slow_array = []
+        slow_array.append(motorstep("fast"))
+        slow_array.append(motorstep("middle")) 
+        slow_array.append(motorstep("slow")) 
+        result = []
+        for f in fast_array:
+            for m in middle_array:
+                for s in slow_array:
+                    result.append(f + m + s)
+        return result
 
-    # step 1:At	time t=perceptualstep0, the stimulus a appears .Nothing has happened in the	brain yet
-    t = start()
 
-    # step 2:peceptial process takes place (arrows around the pecetial process)
-    # a is stored in the visual working memory (a')  and an impression of that os om the working memory a''
-    t += perceptualstep("middle")
+      
 
-    # step 3: cognitive process takes place prepares motor action and yess was diceded
-    t += cognitivestep("middle")
-    # step 4: motor function is executed (presses)
-    t += motorstep("")
-    return t
+
+
+print(example2("all"))
+
