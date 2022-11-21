d56cb1a39b0f042db6527fc314fb56c9b832e7bd
mtdonkers <118730606+mtdonkers@users.noreply.github.com>
2022-11-21 12:28:16 +0100

    Update app.py
    ex 2

--- app.py
+++ app.py
@@ -60,3 +60,55 @@ def example1():
 
 
 print(str(example1())+ "ms")
+
+# Q 1D
+def example2():
+    temp = "extreme"
+    if(temp == "extreme"):
+        t = start()
+        t += perceptualstep("fast")
+        t += cognitivestep("fast")
+        t += motorstep("fast")
+        print(t)
+
+        t = start()
+        t += perceptualstep("middle")
+        t += cognitivestep("middle")
+        t += motorstep("middle")
+        print(t)
+
+        t = start()
+        t += perceptualstep("slow")
+        t += cognitivestep("slow")
+        t += motorstep("slow")
+        print(t)
+    elif(temp == "all"):
+        a = start()
+        test1 = []
+        test1.append(perceptualstep("fast"))
+        test1.append(cognitivestep("fast")) 
+        test1.append(motorstep("fast")) 
+        print(t)
+
+        t += perceptualstep("middle")
+        t += cognitivestep("middle")
+        t += motorstep("middle")
+        print(t)
+
+        t += perceptualstep("slow")
+        t += cognitivestep("slow")
+        t += motorstep("slow")
+        print(t)
+
+    # step 1:At	time t=perceptualstep0, the stimulus a appears .Nothing has happened in the	brain yet
+    t = start()
+
+    # step 2:peceptial process takes place (arrows around the pecetial process)
+    # a is stored in the visual working memory (a')  and an impression of that os om the working memory a''
+    t += perceptualstep("middle")
+
+    # step 3: cognitive process takes place prepares motor action and yess was diceded
+    t += cognitivestep("middle")
+    # step 4: motor function is executed (presses)
+    t += motorstep("")
+    return t
