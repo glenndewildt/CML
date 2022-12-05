bfea56797e976ec186f9cc3b2b2267def554b2b7
glenndewildt <glenn@vergelijkgroep.nl>
2022-12-05 15:55:32 +0100

    Update ex2.py

--- Ex2/ex2.py
+++ Ex2/ex2.py
@@ -37,6 +37,32 @@ for cat in resultZero:
 
 cats = ["an","inAn"]
 
+# Calculate the average
+Pos_mean = numpy.mean(avgPos)
+Zero_mean = numpy.mean(avgZero)
+
+# Calculate the standard deviation
+Pos_std = numpy.std(avgPos)
+Zero_std = numpy.std(avgZero)
+
+# Create lists for the plot
+x_pos = numpy.arange(len(cats))
+CTEs = [Pos_mean, Zero_mean]
+error = [Pos_std, Zero_std]
+
+# Build the plot
+fig, ax = plt.subplots()
+ax.bar(x_pos, CTEs, yerr=error, align='center', alpha=0.5, ecolor='black', capsize=10)
+ax.set_ylabel('Label y')
+ax.set_xticks(x_pos)
+ax.set_xticklabels(cats)
+ax.set_title('label x')
+ax.yaxis.grid(True)
+
+# Save the figure and show
+plt.tight_layout()
+plt.show()
+
 
 
 
