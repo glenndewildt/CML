cb448593de314b11d9c2861b7f8c582a318f3ddc
glenndewildt <glenn@vergelijkgroep.nl>
2022-12-07 15:44:26 +0100

    update

--- Ex2/ex2.py
+++ Ex2/ex2.py
@@ -3,6 +3,8 @@ import math
 import numpy
 import random
 import matplotlib.pyplot as plt
+from scipy import stats
+
 
 
 nr1 = pd.read_csv('NeuralResponses_S1.txt')
@@ -16,14 +18,74 @@ resultZero = []
 index = 0
 
 for value in catVectors.iloc[:,0]:
+
     if value == 0:
         resultZero.append(nr1.iloc[index, :])
 
     if value == 1:
         resultPos.append(nr1.iloc[index, :])
 
-    index +=1
+    index += 1
 
+##########################################
+### 1 B
+##########################################
+index = 0
+PosResult = []
+for x in range(100):
+    PosResult.append([])
+for cat in resultPos:
+    index = 0
+    for c in cat:
+        PosResult[index].append(c)
+        index += 1
+
+print(PosResult[1][0])
+print(PosResult[2][0])
+print(PosResult[0][0])
+
+
+avgRes =[]
+for r in PosResult:
+    avgRes.append(numpy.average(r))
+
+
+ZeroResult = []
+for x in range(100):
+    ZeroResult.append([])
+for cat in resultZero:
+    index = 0
+
+    for c in cat:
+
+
+        ZeroResult[index].append(c)
+        index += 1
+avgPos =[]
+for r in ZeroResult:
+    avgPos.append(numpy.average(r))
+
+
+templist = []
+
+for n in range(20):
+  templist.append(avgRes[n] - avgPos[n])
+print(templist)
+
+# Build the plot
+fig, ax = plt.subplots()
+ax.bar(numpy.arange(20),templist, align='center', alpha=0.5, ecolor='black', capsize=10)
+ax.set_ylabel('Response Aplitude')
+ax.set_title('Voxels')
+ax.yaxis.grid(True)
+
+# Save the figure and show
+plt.tight_layout()
+plt.show()
+
+##########################################
+### 1 A
+##########################################
 avgPos = []
 for cat in resultPos:
     x = numpy.average(cat)
@@ -45,10 +107,15 @@ Zero_mean = numpy.mean(avgZero)
 Pos_std = numpy.std(avgPos)
 Zero_std = numpy.std(avgZero)
 
+
 # Create lists for the plot
 x_pos = numpy.arange(len(cats))
 CTEs = [Pos_mean, Zero_mean]
 error = [Pos_std, Zero_std]
+test = (abs(Pos_mean - Zero_mean))/((abs(Pos_std - Zero_std))/numpy.sqrt(44))
+
+stats.t.ppf(avgPos, 43)
+
 
 # Build the plot
 fig, ax = plt.subplots()
@@ -61,7 +128,7 @@ ax.yaxis.grid(True)
 
 # Save the figure and show
 plt.tight_layout()
-plt.show()
+#plt.show()
 
 
 
